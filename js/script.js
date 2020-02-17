856bb062638c15ff50d3310448e2870affba29be
NikitaSech <NikitaSech@yandex.ru>
2020-02-17 15:14:10 +0300

    Начинаем оформление декоративных элементов

---
+++ js/script.js
@@ -0,0 +1,38 @@
+ymaps.ready(init);
+function init(){
+    var myMap = new ymaps.Map("map", {
+        center: [59.939252, 30.329862],
+        zoom: 16,
+        controls: []
+    });
+
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+
+    myPlacemarkWithContent = new ymaps.Placemark([59.938635, 30.323118], {}, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#imageWithContent',
+        // Своё изображение иконки метки.
+        iconImageHref: 'img/map-mark.png',
+        // Размеры метки.
+        iconImageSize: [218, 142],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-55, -135],
+        // Смещение слоя с содержимым относительно слоя с картинкой.
+        iconContentOffset: [15, 15],
+        // Макет содержимого.
+        iconContentLayout: MyIconContentLayout
+    });
+
+    myMap.geoObjects
+        .add(myPlacemarkWithContent);
+
+    myMap.behaviors
+        // Отключаем часть включенных по умолчанию поведений:
+        //  - drag - перемещение карты при нажатой левой кнопки мыши;
+        //  - magnifier.rightButton - увеличение области, выделенной правой кнопкой мыши.
+        .disable(["scrollZoom"])
+}
